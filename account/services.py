3d3c091854f176e42e7d66ad7beb09b3d18493aa
cwadven <cwadven@naver.com>
2022-12-21 22:14:10 +0900

    feat: nickname 있는지 확인
    - nickname 있는지 확인하는 함수 생성

--- account/services.py
+++ account/services.py
@@ -3,3 +3,7 @@ from account.models import User
 
 def is_username_exists(username):
     return User.objects.filter(username=username).exists()
+
+
+def is_nickname_exists(nickname):
+    return User.objects.filter(nickname=nickname).exists()
--- account/test/test_services.py
+++ account/test/test_services.py
@@ -1,7 +1,7 @@
 from account.models import User
 from django.test import TestCase
 
-from account.services import is_username_exists
+from account.services import is_username_exists, is_nickname_exists
 
 
 class AccountServiceTestCase(TestCase):
@@ -18,3 +18,14 @@ class AccountServiceTestCase(TestCase):
     def test_is_username_exists_should_return_false_when_username_not_exists(self):
         # Expected:
         self.assertFalse(is_username_exists('test'))
+
+    def test_is_nickname_exists_should_return_true_when_username_exists(self):
+        # Given: test 라는 nickname 을 가진 User 생성
+        User.objects.create_user(username='aaaa', nickname='test')
+
+        # Expected:
+        self.assertTrue(is_nickname_exists('test'))
+
+    def test_is_nickname_exists_should_return_false_when_username_not_exists(self):
+        # Expected:
+        self.assertFalse(is_nickname_exists('test'))
