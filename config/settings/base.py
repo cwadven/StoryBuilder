4d96b1718289893c1bca789bd765cb3fcc6fbee0
cwadven <cwadven@naver.com>
2022-12-22 23:16:29 +0900

    feat: smtp 설정
    - gmail smtp 설정

--- config/settings/base.py
+++ config/settings/base.py
@@ -190,3 +190,9 @@ LOGGING = {
         },
     }
 }
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
