dc3c8ef1fc10321abda7cfca2f3b0fde0ef35f2c
cwadven <cwadven@naver.com>
2023-01-08 21:57:57 +0900

    feat: nextsheetpath admin 데이터 설명
    - nextsheetpath 데이터 설명

--- story/admin.py
+++ story/admin.py
@@ -41,10 +41,20 @@ class SheetAnswerAdmin(admin.ModelAdmin):
 class NextSheetPathAdmin(admin.ModelAdmin):
     list_display = [
         'id',
-        'sheet_id',
         'answer_id',
+        'answer_answer',
+        'sheet_id',
+        'sheet_title',
         'quantity',
     ]
+
+    def answer_answer(self, obj):
+        return obj.answer.answer
+    answer_answer.short_description = '정답'
+
+    def sheet_title(self, obj):
+        return obj.sheet.title
+    sheet_title.short_description = '정답을 맞춘 후, 다음 시트 title'
 
 
 admin.site.register(Story, StoryAdmin)
