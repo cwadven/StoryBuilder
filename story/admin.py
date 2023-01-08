f3688e560c8108f0038b365bf1cb8cb910571649
cwadven <cwadven@naver.com>
2023-01-08 22:09:08 +0900

    feat: 링크 추가
    - id 클릭 시 링크 추가

--- story/admin.py
+++ story/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from story.admin_forms import StoryAdminForm, SheetAdminForm
 from story.models import Story, Sheet, SheetAnswer, NextSheetPath
@@ -43,28 +45,45 @@ class NextSheetPathAdmin(admin.ModelAdmin):
         'id',
         'story_id',
         'story_title',
-        'sheet_id',
+        'next_sheet_path_sheet_id',
         'sheet_title',
-        'answer_id',
+        'next_sheet_path_sheet_answer_id',
         'answer_answer',
         'quantity',
     ]
 
-    def answer_answer(self, obj):
-        return obj.answer.answer
-    answer_answer.short_description = '정답'
-
-    def sheet_title(self, obj):
-        return obj.sheet.title
-    sheet_title.short_description = '정답을 맞춘 후, 다음 시트 title'
-
     def story_id(self, obj):
-        return obj.sheet.story_id
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:story_story_change", args=[obj.sheet.story_id]),
+            obj.sheet.story_id
+        ))
     story_id.short_description = 'Story ID'
 
     def story_title(self, obj):
         return obj.sheet.story.title
     story_title.short_description = 'Story title'
+
+    def next_sheet_path_sheet_id(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:story_sheet_change", args=[obj.sheet_id]),
+            obj.sheet_id
+        ))
+    next_sheet_path_sheet_id.short_description = '정답을 맞춘 후, 다음 Sheet ID'
+
+    def sheet_title(self, obj):
+        return obj.sheet.title
+    sheet_title.short_description = '정답을 맞춘 후, 다음 Sheet title'
+
+    def next_sheet_path_sheet_answer_id(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:story_sheetanswer_change", args=[obj.answer_id]),
+            obj.answer_id
+        ))
+    next_sheet_path_sheet_answer_id.short_description = '정답 ID'
+
+    def answer_answer(self, obj):
+        return obj.answer.answer
+    answer_answer.short_description = '정답'
 
     def get_queryset(self, request):
         return super(NextSheetPathAdmin, self).get_queryset(
