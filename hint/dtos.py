2d920eb9d4bbf71e8df3e81d3027bdbe21616ebb
cwadven <cwadven@naver.com>
2023-01-21 22:19:21 +0900

    feat: sheet hint dto 생성
    - sheet hint dto 생성
    - 테스트케이스 생성

---
+++ hint/dtos.py
@@ -0,0 +1,25 @@
+import attr
+
+from hint.models import SheetHint
+
+
+@attr.s
+class UserSheetHintInfoDTO(object):
+    id = attr.ib(type=int)
+    hint = attr.ib(type=str)
+    point = attr.ib(type=int)
+    image = attr.ib(type=str)
+    has_history = attr.ib(type=bool)
+
+    @classmethod
+    def of(cls, sheet_hint: SheetHint, has_history: bool):
+        return cls(
+            id=sheet_hint.id,
+            hint=sheet_hint.hint if has_history else '',
+            point=sheet_hint.point,
+            image=sheet_hint.image if has_history else '',
+            has_history=has_history,
+        )
+
+    def to_dict(self):
+        return attr.asdict(self, recurse=True)
