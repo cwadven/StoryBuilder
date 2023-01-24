f698f1ef1301183b5ca2e11086eb1ac19bbc41fe
cwadven <cwadven@naver.com>
2023-01-24 16:34:41 +0900

    feat: point model 생성
    - point model 생성

--- point/models.py
+++ point/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+
+
+class UserPoint(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
+    point = models.IntegerField(verbose_name='포인트')
+    description = models.CharField(verbose_name='생성된 이유', max_length=120)
+    is_active = models.BooleanField(verbose_name='유효성', default=True, db_index=True)
+    created_at = models.DateTimeField(verbose_name='생성일', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='수정일', auto_now=True)
