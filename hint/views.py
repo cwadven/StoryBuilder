3902986f4f502f8b7c5a26ca4d932ce8a96c70c9
cwadven <cwadven@naver.com>
2023-01-24 15:57:16 +0900

    feat: sheet hint 유저에게 등록하는 api 생성
    - 등록 api 생성

--- hint/views.py
+++ hint/views.py
@@ -1,10 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from common_decorator import custom_login_required_for_method
+from common_decorator import custom_login_required_for_method, mandatories
 from story.services import get_running_sheet
-from .dtos import UserSheetHintInfosResponse
-from .services import get_sheet_hint_infos
+from .dtos import UserSheetHintInfosResponse, UserSheetHintInfoDTO
+from .services import get_sheet_hint_infos, give_sheet_hint_information, get_available_sheet_hint
 
 
 class SheetHintAPIView(APIView):
@@ -17,3 +17,16 @@ class SheetHintAPIView(APIView):
             user_sheet_hint_infos=sheet_hint_infos,
         ).to_dict()
         return Response(user_sheet_hint_infos, status=200)
+
+    @mandatories('sheet_hint_id')
+    @custom_login_required_for_method
+    def post(self, request, sheet_id, m):
+        get_running_sheet(sheet_id)
+        available_sheet_hint = get_available_sheet_hint(m['sheet_hint_id'])
+
+        sheet_hint = give_sheet_hint_information(
+            user_id=request.user.id,
+            sheet_hint_id=available_sheet_hint.id,
+        )
+        sheet_hint_info = UserSheetHintInfoDTO.of(sheet_hint, True).to_dict()
+        return Response(sheet_hint_info, status=200)
