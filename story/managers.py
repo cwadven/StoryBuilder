831b07fad9333b6fb707b25a6ae74aa73102fb97
cwadven <cwadven@naver.com>
2023-03-25 22:56:12 +0900

    fix: 버그 수정
    - 버그 수정

--- story/managers.py
+++ story/managers.py
@@ -1,22 +1,40 @@
-from django.db.models import Manager, Q
+from django.db.models import Manager, Q, Exists, OuterRef
 
 
 class StoryManager(Manager):
     def get_actives(self, user=None):
-        return self.filter(
+        base_query = self.filter(
             is_deleted=False,
             displayable=True,
-        ).filter(
-            Q(is_secret=False) | Q(secret_members=user),
         )
+        if user:
+            secret_member_condition = Exists(
+                user.secret_stories.filter(pk=OuterRef('pk'))
+            )
+            base_query = base_query.filter(
+                Q(is_secret=False) | secret_member_condition,
+            )
+        else:
+            base_query = base_query.filter(is_secret=False)
+
+        return base_query
 
 
 class PopularStoryManager(Manager):
     def get_actives(self, user=None):
-        return self.filter(
+        base_query = self.filter(
             story__is_deleted=False,
             story__displayable=True,
             is_deleted=False,
-        ).filter(
-            Q(story__is_secret=False) | Q(story__secret_members=user),
         )
+        if user:
+            secret_member_condition = Exists(
+                user.secret_stories.filter(pk=OuterRef('story_id'))
+            )
+            base_query = base_query.filter(
+                Q(story__is_secret=False) | secret_member_condition,
+            )
+        else:
+            base_query = base_query.filter(story__is_secret=False)
+
+        return base_query
