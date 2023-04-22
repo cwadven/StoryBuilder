840d8e12afce4ddc4fc0c163962c41f5728fb1f9
cwadven <cwadven@naver.com>
2023-04-23 01:20:07 +0900

    feat: is_always_correct 추가
    - is_always_correct 추가

--- story/dtos.py
+++ story/dtos.py
@@ -10,6 +10,7 @@ class SheetAnswerResponseDTO(object):
     id = attr.ib(type=int)
     answer = attr.ib(type=str)
     answer_reply = attr.ib(type=str)
+    is_always_correct = attr.ib(type=bool)
     next_sheet_path_id = attr.ib(type=int)
     next_sheet_id = attr.ib(type=int)
     next_sheet_quantity = attr.ib(type=int)
@@ -20,6 +21,7 @@ class SheetAnswerResponseDTO(object):
             id=sheet_answer['id'],
             answer=sheet_answer['answer'].replace(' ', ''),
             answer_reply=sheet_answer['answer_reply'],
+            is_always_correct=sheet_answer['is_always_correct'],
             next_sheet_path_id=sheet_answer['nextsheetpath'],
             next_sheet_id=sheet_answer['next_sheet_paths__nextsheetpath__sheet_id'],
             next_sheet_quantity=sheet_answer['next_sheet_paths__nextsheetpath__quantity'],
