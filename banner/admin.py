ed02aa9c2c7be60a48c65f0bae81d8af031d266c
cwadven <cwadven@naver.com>
2023-04-30 17:32:35 +0900

    feat: Admin 정의
    - Admin 정의

--- banner/admin.py
+++ banner/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.urls import reverse
 
-# Register your models here.
+from banner.models import BannerType, Banner
+
+
+class BannerTypeAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'description',
+    ]
+
+
+class BannerAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'description',
+        'banner_type_name',
+        'sequence',
+        'start_time',
+        'end_time',
+        'is_active',
+    ]
+
+    def get_queryset(self, request):
+        return super(BannerAdmin, self).get_queryset(
+            request
+        ).select_related(
+            'banner_type',
+        )
+
+    def banner_type_name(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:banner_bannertype_change", args=[obj.banner_type_id]),
+            obj.banner_type.name,
+        ))
+    banner_type_name.short_description = '배너 타입'
+
+
+admin.site.register(BannerType, BannerTypeAdmin)
+admin.site.register(Banner, BannerAdmin)
