8609aaad8fa05c1b13615af3e15867b744dc7e9e
cwadven <cwadven@naver.com>
2023-04-30 21:06:26 +0900

    feat: Banner Detail dto 생성
    - Banner Detail dto 생성

--- banner/dtos.py
+++ banner/dtos.py
@@ -23,3 +23,29 @@ class BannerListItemDTO(object):
 
     def to_dict(self):
         return attr.asdict(self, recurse=True)
+
+
+@attr.s
+class BannerDetailItemDTO(object):
+    id = attr.ib(type=int)
+    title = attr.ib(type=str)
+    description = attr.ib(type=str)
+    background_image = attr.ib(type=str)
+    background_color = attr.ib(type=str)
+    banner_type_name = attr.ib(type=str)
+    created_at = attr.ib(type=str)
+
+    @classmethod
+    def of(cls, banner: Banner):
+        return cls(
+            id=banner.id,
+            title=banner.title,
+            description=banner.description,
+            background_image=banner.background_image,
+            background_color=banner.background_color,
+            banner_type_name=banner.banner_type.name,
+            created_at=banner.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+        )
+
+    def to_dict(self):
+        return attr.asdict(self, recurse=True)
--- banner/test/test_dtos.py
+++ banner/test/test_dtos.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 from django.test import TestCase
 
-from banner.dtos import BannerListItemDTO
+from banner.dtos import BannerListItemDTO, BannerDetailItemDTO
 from banner.models import BannerType, Banner
 
 
@@ -32,3 +32,33 @@ class BannerListItemDTOTestCase(TestCase):
         self.assertEqual(banner_list_item['background_image'], self.banner.background_image)
         self.assertEqual(banner_list_item['background_color'], self.banner.background_color)
         self.assertEqual(banner_list_item['banner_type_name'], self.banner.banner_type.name)
+
+
+class BannerDetailItemDTOTestCase(TestCase):
+    def setUp(self):
+        self.banner_type = BannerType.objects.create(name='Test Banner Type', description='Test Description')
+        self.banner = Banner.objects.create(
+            title='Active Banner (no end time)',
+            description='Test Description',
+            background_image='https://example.com/banner-image.jpg',
+            background_color='#000000',
+            banner_type=self.banner_type,
+            sequence=1,
+            start_time=datetime.now() - timedelta(days=1),
+            is_active=True,
+        )
+
+    def test_banner_detail_item_dto(self):
+        # Given:
+        # When: dto 객체 생성
+        banner_detail_item_dto = BannerDetailItemDTO.of(self.banner)
+        banner_detail_item = banner_detail_item_dto.to_dict()
+
+        # Then: set dto
+        self.assertEqual(banner_detail_item['id'], self.banner.id)
+        self.assertEqual(banner_detail_item['title'], self.banner.title)
+        self.assertEqual(banner_detail_item['description'], self.banner.description)
+        self.assertEqual(banner_detail_item['background_image'], self.banner.background_image)
+        self.assertEqual(banner_detail_item['background_color'], self.banner.background_color)
+        self.assertEqual(banner_detail_item['banner_type_name'], self.banner.banner_type.name)
+        self.assertEqual(banner_detail_item['created_at'], self.banner.created_at.strftime('%Y-%m-%d %H:%M:%S'))
