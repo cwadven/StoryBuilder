62375e9c806f1514802f061d8b4dba1cb4eadeef
cwadven <cwadven@naver.com>
2023-04-30 21:24:33 +0900

    feat: get banner detail api 조회 생성
    - get banner detail api 조회 생성

--- banner/urls.py
+++ banner/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from banner.views import BannerListAPIView
-
+from banner.views import BannerListAPIView, BannerDetailAPIView
 
 app_name = 'banner'
 
 urlpatterns = [
     path('', BannerListAPIView.as_view(), name='banner_list'),
+    path('<int:banner_id>', BannerDetailAPIView.as_view(), name='banner_detail'),
 ]
--- banner/views.py
+++ banner/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from banner.dtos import BannerListItemDTO
+from banner.dtos import BannerListItemDTO, BannerDetailItemDTO
+from banner.models import Banner
 from banner.services import get_active_banners
+from config.common.exception_codes import BannerDoesNotExists
 
 
 class BannerListAPIView(APIView):
@@ -15,5 +17,18 @@ class BannerListAPIView(APIView):
                     for banner in active_banners_order_by_sequence_desc
                 ]
             },
+            status=200,
+        )
+
+
+class BannerDetailAPIView(APIView):
+    def get(self, request, banner_id):
+        try:
+            banner = get_active_banners().get(id=banner_id)
+        except Banner.DoesNotExist:
+            raise BannerDoesNotExists()
+
+        return Response(
+            data=BannerDetailItemDTO.of(banner).to_dict(),
             status=200,
         )
