c66141d6873f0d98520d0b6847918120f052fadb
cwadven <cwadven@naver.com>
2023-04-30 21:12:08 +0900

    feat: get_active_banner 조회 생성
    - get_active_banner 조회 1개 생성

--- banner/services.py
+++ banner/services.py
@@ -2,10 +2,13 @@ from datetime import datetime
 
 from typing import List
 
+from django.db.models import QuerySet
+
 from banner.models import Banner
+from config.common.exception_codes import BannerDoesNotExists
 
 
-def get_active_banners(now=None, order_by: List[str] = None) -> List[Banner]:
+def get_active_banners(now=None, order_by: List[str] = None) -> QuerySet[Banner]:
     if now is None:
         now = datetime.now()
 
@@ -14,3 +17,13 @@ def get_active_banners(now=None, order_by: List[str] = None) -> List[Banner]:
         banner_qs = banner_qs.order_by(*order_by)
 
     return banner_qs
+
+
+def get_active_banner(banner_id: int, now=None) -> Banner:
+    if now is None:
+        now = datetime.now()
+
+    try:
+        return get_active_banners(now=now).get(id=banner_id)
+    except Banner.DoesNotExist:
+        raise BannerDoesNotExists()
--- banner/test/test_services.py
+++ banner/test/test_services.py
@@ -3,7 +3,8 @@ from datetime import datetime, timedelta
 from django.test import TestCase
 
 from banner.models import BannerType, Banner
-from banner.services import get_active_banners
+from banner.services import get_active_banners, get_active_banner
+from config.common.exception_codes import BannerDoesNotExists
 
 
 class GetActiveBannersTestCase(TestCase):
@@ -43,3 +44,22 @@ class GetActiveBannersTestCase(TestCase):
         active_banners = get_active_banners(now=now, order_by=['-sequence'])
         # Expected: sequence 역정렬 순으로 생성
         self.assertEqual(list(active_banners), [self.banner1, self.banner2])
+
+    def test_get_active_banner_should_return_banner(self):
+        # Given:
+        now = datetime.now()
+
+        # When:
+        active_banner = get_active_banner(banner_id=self.banner1.id, now=now)
+
+        # Then: banner1 반환
+        self.assertEqual(active_banner.id, self.banner1.id)
+
+    def test_get_active_banner_should_raise_error_when_banner_not_exists(self):
+        # Given: banner1 비 활성화
+        self.banner1.is_active = False
+        self.banner1.save()
+
+        # Expected: BannerDoesNotExists 에러 반환
+        with self.assertRaises(BannerDoesNotExists):
+            get_active_banner(banner_id=self.banner1.id)
