1c9a76212feff8a61c8e90b2211010c9252e98fd
cwadven <cwadven@naver.com>
2023-05-28 17:48:29 +0900

    feat: Create new api for cancel
    - for cancel

--- payment/urls.py
+++ payment/urls.py
@@ -1,7 +1,11 @@
 from django.http import JsonResponse
 from django.urls import path
 
-from payment.views import KakaoPayReadyForBuyPointAPIView, KakaoPayApproveForBuyPointAPIView
+from payment.views import (
+    KakaoPayApproveForBuyPointAPIView,
+    KakaoPayCancelForBuyPointAPIView,
+    KakaoPayReadyForBuyPointAPIView,
+)
 
 app_name = 'payment'
 
@@ -12,4 +16,5 @@ urlpatterns = [
     path('test_fail', lambda request: JsonResponse({'message': 'fail'}), name='test'),
     path('point/buy', KakaoPayReadyForBuyPointAPIView.as_view(), name='point_buy'),
     path('point/approve/<int:order_id>', KakaoPayApproveForBuyPointAPIView.as_view(), name='point_approve'),
+    path('point/cancel/<int:order_id>', KakaoPayCancelForBuyPointAPIView.as_view(), name='point_cancel'),
 ]
