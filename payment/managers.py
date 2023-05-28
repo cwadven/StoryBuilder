0484b0f6334a70911d54d532118ccb897c884819
cwadven <cwadven@naver.com>
2023-05-28 20:44:54 +0900

    feat: Add additional product manager
    - Add additional product manager

--- payment/managers.py
+++ payment/managers.py
@@ -8,8 +8,21 @@ class ProductManager(Manager):
             now = timezone.now()
 
         return self.filter(
-            (Q(start_time__lte=now, end_time__gte=now) | Q(end_time__isnull=True)),
+            (Q(start_time__lte=now) | Q(start_time__isnull=True)),
+            (Q(end_time__gte=now) | Q(end_time__isnull=True)),
             is_active=True,
             quantity__gt=0,
             is_sold_out=False,
+        )
+
+
+class AdditionalProductManager(Manager):
+    def get_actives(self, now=None):
+        if now is None:
+            now = timezone.now()
+
+        return self.filter(
+            (Q(start_time__lte=now) | Q(start_time__isnull=True)),
+            (Q(end_time__gte=now) | Q(end_time__isnull=True)),
+            is_active=True,
         )
--- payment/test/test_managers.py
+++ payment/test/test_managers.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.utils import timezone
 
-from payment.models import PointProduct
+from payment.models import PointProduct, AdditionalPointProduct
 
 
 class TestProductManager(TestCase):
@@ -88,3 +88,86 @@ class TestProductManager(TestCase):
 
         # Then:
         self.assertNotIn(self.inactive_product_sold_out, active_products)
+
+
+class TestAdditionalProductManager(TestCase):
+    def setUp(self):
+        # Set up data for the test
+        self.active_product = PointProduct.objects.create(
+            title='Active Product',
+            price=1000,
+            start_time=timezone.now() - timezone.timedelta(hours=1),
+            end_time=timezone.now() + timezone.timedelta(hours=1),
+            quantity=10,
+            point=1000,
+        )
+
+        self.active_additional_product = AdditionalPointProduct.objects.create(
+            point_product=self.active_product,
+            description='Active Product',
+            price=1000,
+            start_time=timezone.now() - timezone.timedelta(hours=1),
+            end_time=timezone.now() + timezone.timedelta(hours=1),
+            point=1000,
+        )
+
+        self.inactive_additional_product_future = AdditionalPointProduct.objects.create(
+            point_product=self.active_product,
+            description='Inactive Product Future',
+            price=1000,
+            start_time=timezone.now() + timezone.timedelta(hours=1),
+            end_time=timezone.now() + timezone.timedelta(hours=2),
+            point=1000,
+        )
+
+        self.inactive_additional_product_past = AdditionalPointProduct.objects.create(
+            point_product=self.active_product,
+            description='Inactive Product Past',
+            price=1000,
+            start_time=timezone.now() - timezone.timedelta(hours=2),
+            end_time=timezone.now() - timezone.timedelta(hours=1),
+            point=1000,
+        )
+
+        self.inactive_additional_product_is_not_active = AdditionalPointProduct.objects.create(
+            point_product=self.active_product,
+            description='Inactive Product deactivate',
+            price=1000,
+            start_time=timezone.now() - timezone.timedelta(hours=1),
+            end_time=timezone.now() + timezone.timedelta(hours=1),
+            is_active=False,
+            point=1000,
+        )
+
+    def test_get_actives(self):
+        # Test get_actives method
+        # When: 실행
+        active_additional_products = AdditionalPointProduct.objects.get_actives()
+
+        # Then:
+        self.assertEqual(active_additional_products.count(), 1)
+        self.assertEqual(active_additional_products.first(), self.active_additional_product)
+
+    def test_inactive_product_future(self):
+        # Check that the product with future start time is not included
+        # When: 실행
+        additional_products = AdditionalPointProduct.objects.get_actives()
+
+        # Then:
+        self.assertNotIn(self.inactive_additional_product_future, additional_products)
+
+    def test_inactive_product_past(self):
+        # Check that the product with past end time is not included
+        # When: 실행
+        additional_products = AdditionalPointProduct.objects.get_actives()
+
+        # Then:
+        self.assertNotIn(self.inactive_additional_product_past, additional_products)
+
+    def test_inactive_product_is_not_active(self):
+        # Check that the sold out product is not included
+        # When: 실행
+        additional_products = AdditionalPointProduct.objects.get_actives()
+
+        # Then:
+        self.assertNotIn(self.inactive_additional_product_is_not_active, additional_products)
