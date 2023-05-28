159bf37d40406dedbec8b5a5bae5da3364c75b84
cwadven <cwadven@naver.com>
2023-05-28 20:58:31 +0900

    feat: Add dtos
    - Add dtos

--- payment/dtos.py
+++ payment/dtos.py
@@ -1,9 +1,15 @@
 import attr
+from typing import List
+
+from django.db.models import Sum
+
+from payment.consts import ProductType
+from payment.models import PointProduct
 
 
 @attr.s
 class PointPayReadyRequestDTO(object):
-    product_id = attr.ib(type=id)
+    product_id = attr.ib(type=int)
     partner_order_id = attr.ib(type=str)
     partner_user_id = attr.ib(type=str)
     item_name = attr.ib(type=str)
@@ -23,6 +29,69 @@ class KakaoPayReadyResponseDTO(object):
         return cls(
             tid=data['tid'],
             next_redirect_mobile_url=data['next_redirect_mobile_url'],
+        )
+
+    def to_dict(self):
+        return attr.asdict(self, recurse=True)
+
+
+@attr.s
+class AdditionalPointProductItemDTO(object):
+    description = attr.ib(type=str)
+    price = attr.ib(type=str)
+    point = attr.ib(type=str)
+
+    def to_dict(self):
+        return attr.asdict(self, recurse=True)
+
+
+@attr.s
+class PointProductItemDTO(object):
+    product_id = attr.ib(type=int)
+    product_type = attr.ib(type=str)
+    title = attr.ib(type=str)
+    description = attr.ib(type=str)
+    image = attr.ib(type=str)
+    total_price = attr.ib(type=int)
+    price = attr.ib(type=int)
+    total_point = attr.ib(type=int)
+    point = attr.ib(type=int)
+    is_sold_out = attr.ib(type=bool)
+    bought_count = attr.ib(type=int)
+    review_count = attr.ib(type=int)
+    review_rate = attr.ib(type=float)
+    additional_products = attr.ib(type=List[AdditionalPointProductItemDTO])
+
+    @classmethod
+    def of(cls, point_product: PointProduct) -> 'PointProductItemDTO':
+        additional_products = []
+        total_price = point_product.price
+        total_point = point_product.price
+        for additional_product in point_product.additionalpointproduct_set.get_actives():
+            total_price += additional_product.price
+            total_point += additional_product.point
+            additional_products.append(
+                AdditionalPointProductItemDTO(
+                    description=additional_product.description,
+                    price=additional_product.price,
+                    point=additional_product.point,
+                )
+            )
+        return cls(
+            product_id=point_product.id,
+            product_type=ProductType.POINT.value,
+            title=point_product.title,
+            description=point_product.description,
+            image=point_product.image,
+            total_price=total_price,
+            price=point_product.price,
+            total_point=total_point,
+            point=point_product.point,
+            is_sold_out=point_product.is_sold_out,
+            bought_count=point_product.bought_count,
+            review_count=point_product.review_count,
+            review_rate=point_product.review_rate,
+            additional_products=additional_products,
         )
 
     def to_dict(self):
---
+++ payment/test/test_dtos.py
@@ -0,0 +1,65 @@
+from django.test import TestCase
+from django.utils import timezone
+
+from payment.consts import ProductType
+from payment.dtos import PointProductItemDTO
+from payment.models import PointProduct, AdditionalPointProduct
+
+
+class PointProductItemDTOTestCase(TestCase):
+    def setUp(self):
+        self.active_product = PointProduct.objects.create(
+            title='Active Product',
+            price=1000,
+            start_time=timezone.now() - timezone.timedelta(hours=1),
+            end_time=timezone.now() + timezone.timedelta(hours=1),
+            quantity=10,
+            point=1000,
+        )
+        self.active_additional_product = AdditionalPointProduct.objects.create(
+            point_product=self.active_product,
+            description='Active Product',
+            price=1000,
+            start_time=timezone.now() - timezone.timedelta(hours=1),
+            end_time=timezone.now() + timezone.timedelta(hours=1),
+            point=1000,
+        )
+
+    def test_point_product_item_dto_when_additional_point_product_exists(self):
+        # Given:
+        # When:
+        point_product_item_dto = PointProductItemDTO.of(self.active_product)
+
+        # Then: product
+        self.assertEquals(point_product_item_dto.product_id, self.active_product.id)
+        self.assertEquals(point_product_item_dto.product_type, ProductType.POINT.value)
+        self.assertEquals(point_product_item_dto.title, self.active_product.title)
+        self.assertEquals(point_product_item_dto.description, self.active_product.description)
+        self.assertEquals(point_product_item_dto.image, self.active_product.image)
+        self.assertEquals(
+            point_product_item_dto.total_price,
+            self.active_product.price + self.active_additional_product.price,
+        )
+        self.assertEquals(point_product_item_dto.price, self.active_product.price)
+        self.assertEquals(
+            point_product_item_dto.total_point,
+            self.active_product.point + + self.active_additional_product.point,
+        )
+        self.assertEquals(point_product_item_dto.point, self.active_product.point)
+        self.assertEquals(point_product_item_dto.is_sold_out, self.active_product.is_sold_out)
+        self.assertEquals(point_product_item_dto.bought_count, self.active_product.bought_count)
+        self.assertEquals(point_product_item_dto.review_count, self.active_product.review_count)
+        self.assertEquals(point_product_item_dto.review_rate, self.active_product.review_rate)
+        # And: additional_product
+        self.assertEquals(
+            point_product_item_dto.additional_products[0].point,
+            self.active_additional_product.point,
+        )
+        self.assertEquals(
+            point_product_item_dto.additional_products[0].price,
+            self.active_additional_product.price,
+        )
+        self.assertEquals(
+            point_product_item_dto.additional_products[0].description,
+            self.active_additional_product.description,
+        )
