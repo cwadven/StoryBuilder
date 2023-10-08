4287b7a9eb3904a8f67e1e8be0ec9050ff8633a1
cwadven <cwadven@naver.com>
2023-10-08 20:44:41 +0900

    Feat: Get CMS Story Sheet Map
    - Get CMS Story Sheet Map

--- story/cms_urls.py
+++ story/cms_urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from story.cms_views import CMSStoryListAPIView
+from story.cms_views import (
+    CMSStoryListAPIView,
+    CMSStorySheetMapAPIView,
+)
 
 app_name = 'cms_story'
 
 
 urlpatterns = [
     path('', CMSStoryListAPIView.as_view(), name='story_cms'),
+    path('<int:story_id>/sheet/map', CMSStorySheetMapAPIView.as_view(), name='sheet_cms_map'),
 ]
