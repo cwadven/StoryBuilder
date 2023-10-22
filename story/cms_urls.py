1a4ad727fe4f954a14b7cdb8c9aa9451f59b07b7
cwadven <cwadven@naver.com>
2023-10-22 17:12:48 +0900

    Feat: CMS answer 가져오는 API
    - CMS answer 가져오는 API

--- story/cms_urls.py
+++ story/cms_urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from story.cms_views import (
     CMSStoryListAPIView,
+    CMSStorySheetAnswerMapAPIView,
     CMSStorySheetMapAPIView,
 )
 
@@ -11,4 +12,5 @@ app_name = 'cms_story'
 urlpatterns = [
     path('', CMSStoryListAPIView.as_view(), name='story_cms'),
     path('<int:story_id>/sheet/map', CMSStorySheetMapAPIView.as_view(), name='sheet_cms_map'),
+    path('<int:story_id>/answer/map', CMSStorySheetAnswerMapAPIView.as_view(), name='answer_cms_map'),
 ]
--- story/cms_views.py
+++ story/cms_views.py
@@ -7,12 +7,15 @@ from hint.services import get_available_sheet_hints_count
 from story.cmd_dtos import (
     CMSStoryListItemDTO,
     CMSStoryListResponse,
+    CMSStorySheetAnswerMapItemDTO,
+    CMSStorySheetAnswerMapResponse,
     CMSStorySheetMapItemDTO,
     CMSStorySheetMapResponse,
 )
 from story.cms_services import (
     get_active_sheets,
     get_sheet_answer_ids_by_sheet_ids,
+    get_sheet_answers_by_sheet_ids,
     get_stories_qs,
     get_story_search_filter,
 )
@@ -65,6 +68,23 @@ class CMSStorySheetMapAPIView(APIView):
                         answer_ids=sheet_answer_ids_by_sheet_id[sheet.id],
                     )
                     for sheet in active_sheet_qs
+                ],
+            ).to_dict(),
+            status=200,
+        )
+
+
+class CMSStorySheetAnswerMapAPIView(APIView):
+    permission_classes = [CMSUserPermission]
+
+    def get(self, request, story_id):
+        return Response(
+            data=CMSStorySheetAnswerMapResponse(
+                answers=[
+                    CMSStorySheetAnswerMapItemDTO.of(sheet_answer=sheet_answer)
+                    for sheet_answer in get_sheet_answers_by_sheet_ids(
+                        [sheet.id for sheet in get_active_sheets(story_id)]
+                    )
                 ],
             ).to_dict(),
             status=200,
